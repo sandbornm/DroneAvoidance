5a542b977a84fffbe3cf5f15921013a04b5f690b
sandbornm <michael.sandborn@vanderbilt.edu>
2021-11-14 15:15:55 -0600

    less borken

--- lcp_solve.py
+++ lcp_solve.py
@@ -1,4 +1,3 @@
-import nashpy as nash
 import numpy as np
 import copy
 import matplotlib.pyplot as plt
@@ -6,14 +5,13 @@ import matplotlib.cm as cm
 import matplotlib.animation as animation
 
 from generate_traj import generate_traj
-
-from HW2 import LCP_lemke_howson
+from lemke_howson import LCP_lemke_howson
 
 
 """ hyperparams """
-T = 3 # game length
-L = 3 # turn length
-H = 20 # predictive horizon
+T = 30 # game length
+L = 5 # turn length
+H = 10 # predictive horizon
 nT = int(T/L) # number of turns in game
 N = 5 # number of candidate trajectories
 dof = 8
@@ -27,7 +25,7 @@ dt = .5
 
 """ check if a single pair of trajectories a and b intersect at any point 
     to be used to weight against collision """
-def intersect(a, b, tol=.3): # TODO adjust tol as needed
+def intersect(a, b, tol=1): # TODO adjust tol as needed
     assert a.shape == (dof, H+1) and b.shape == (dof, H+1)
     dists = []
     for h in range(H):
@@ -49,29 +47,52 @@ def distToGoal(traj, player="a"):
     dist = min(dist, np.linalg.norm(traj[:3, H] - goal))
     return dist
 
+"""
+takes the vector of 4D control inputs over the H steps of the computed trajectory
+"""
+def accPenalty(accs):
+    print("in accpenalty")
+    print("accs.shape")
+    print(accs.shape)
+    avAcc = np.average(accs, axis=1)
+    print(avAcc.shape)
+    print(avAcc.T.dot(avAcc))
+    return avAcc.T.dot(avAcc)
 
-""" get cost functions to be solved for each"""
-def getCostMats(Za, Zb):
+
+
+""" get cost functions to be solved for each
+
+Za: the state vector for A 
+Ua: the control input for A
+Same for Zb, Ub
+"""
+def getCostMats(Za, Ua, Zb, Ub):
     mats = []
+
     # for turn in range(nT):
     A = np.zeros((N, N))
     B = np.zeros((N, N))
     for i in range(N):
         for j in range(N): # TODO are these costs symmetric?
             #print(f"Za shape {Za[i, :, :].shape}")
-            A[i, j] = distToGoal(Za[i, :, :]) + (1e6 * intersect(Za[i, :, :], Zb[j, :, :]))
-            B[i, j] = distToGoal(Zb[i, :, :], player="b") + (1e6 * intersect(Za[j, :, :], Zb[i, :, :]))
+            A[i, j] = distToGoal(Za[i, :, :]) + (1e6 * intersect(Za[i, :, :], Zb[j, :, :]))  +(.5 * accPenalty(Ua[i, :, :]))
+            B[i, j] = distToGoal(Zb[i, :, :], player="b") + (1e6 * intersect(Za[j, :, :], Zb[i, :, :])) + (.5 * accPenalty(Ub[i, :, :]))
     mats.append([A, B])
     return np.array(mats)
 
 
 """" solve the LCPs given by A and B at each turn in the game using nashpy """
 def solveLCPs(mats):
+    print("in solve LCPS")
+    print(type(mats))
+    print(type(mats[0]))
     sols = []
-    for [A,B] in list(mats):
-        gme = nash.Game(A, B) # todo how to deal with this
-        sols.append(gme.lemke_howson(initial_dropped_label=0))
-        #sols.append(LCP_lemke_howson(A, B))
+    for x in list(mats):
+        #print(x[0], x[1])
+        #gme = nash.Game(x[1], x[0])
+        #sols.append(lemke_howson_lex(x[0], x[1]))
+        sols.append(LCP_lemke_howson(x[0], x[1]))
     return np.array(sols)
 
 
@@ -79,73 +100,156 @@ def solveLCPs(mats):
 xcur:
 goal: x,y,z goal for player
 """
-def generateTrajectories(start, goal, cx, cy, cz):
-    traj = []
-    for _ in range(nT):
-        for i in range(N):
-            t = generate_traj(start, goal, H, dt, cx[i], cy[i], cz[i])
-            traj.append(t)
-    return np.array(traj)
+def generateTrajectories(start, vstart, goal, cx, cy, cz):
+    traj, acc = [], []
+    for i in range(N):
+        state, control = generate_traj(start, vstart, goal, H, dt, cx[i], cy[i], cz[i])
+        traj.append(state)
+        acc.append(control)
+    return np.array(traj), np.array(acc)
+
+
+
 
 
 """ visualize the results of the simulation """
-def sim():
-    fig, ax = plt.subplots()
-    # pos_a = ax.scatter(Zactual_a[0, 0], Zactual_a[1, 0], 10, 'b')
-    # pos_b = ax.scatter(Zactual_b[0,0], Zactual_b[1, 0], 10, 'r')
+def sim(startA, goalA, startB, goalB, cx, cy, cz):
+    
+    # initial start
 
-    def animate(t):
-        pass
-        # pos_a.set_offsets([Zactual_a[0, t], Zactual_a[1, t]])
-        # pos_b.set_offsets([Zactual_b[0, t], Zactual_b[1, t]])
+    print("startA")
+    print(startA)
+    print("startB")
+    print(startB)
+    nextStartA = startA
+    nextStartB = startB
+    vStartA = [0,0,0]
+    vStartB = [0,0,0]
 
-    ani = animation.FuncAnimation(fig,
-                                    animate,
-                                    save_count=T,  # total number of calls to animate
-                                    interval=dt * 1000)  # interval = miliseconds between frames
-    ani.save("tag.mp4")
+    # store actual chosen trajectories
+    trajAactual = np.zeros((dof, T))
+    trajBactual = np.zeros((dof, T))
+
+    for turn in range(nT):
+
+        print(f"nextStartA {nextStartA}")
+        print(f"nextStartB {nextStartB}")
+
+        trajA, accA = generateTrajectories(nextStartA, vStartA, goalA, cx, cy, cz)
+        trajB, accB = generateTrajectories(nextStartB, vStartB, goalB, cx, cy, cz)
+
+        # print("traj and acc shapes")
+        # print(f"trajA {trajA.shape} accA {accA.shape}")
+        # only consider first 10 points
+        costs = getCostMats(trajA, accA, trajB, accB)
+
+        sols = solveLCPs(costs)
+
+        #print(f"sols shape {sols.shape}")
+
+        # choose action
+        asol = np.argmax(sols[0,0])
+        bsol = np.argmax(sols[0,1])
+        #print(asol, bsol)
+
+        # update actual trajectories
+        trajAactual[:, turn*L:(turn+1)*L] = trajA[asol, :, 0:L]
+        trajBactual[:, turn*L:(turn+1)*L] = trajB[bsol, :, 0:L]
+
+        # update next start position
+        nextStartA = trajA[asol, :3, L] # assign next start to xyz
+        nextStartB = trajB[bsol, :3, L]
+
+        # update next start velocity
+        vStartA = trajA[asol, 4:7, L] # assign next velocity to xyz
+        vStartB = trajB[bsol, 4:7, L]
+
+        print("actual traj") # TODO add error in each direction
+        print(f"A {trajAactual[:3, -1]}")
+        print(f"B {trajBactual[:3, -1]}")
+
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        #ax.plot(trajA[asol, 0, :], trajA[asol, 1, :], trajA[asol, 2, :], label="A")
+        #ax.plot(trajB[bsol, 0, :], trajB[bsol, 1, :], trajB[bsol, 2, :], label="B")
+
+        ax.plot(trajAactual[0, :], trajAactual[1, :], trajAactual[2, :], label="A")
+        ax.plot(trajBactual[0, :], trajBactual[1, :], trajBactual[2, :], label="B")
+        ax.legend(loc="upper left")
+        plt.show()
+
+
+
+
+
+
+    # TODO animation
+    # TODO % error in each direction
+    # TODO other metrics for evaluation?
+
+    # fig, ax = plt.subplots()
+    # # pos_a = ax.scatter(Zactual_a[0, 0], Zactual_a[1, 0], 10, 'b')
+    # # pos_b = ax.scatter(Zactual_b[0,0], Zactual_b[1, 0], 10, 'r')
+
+    # def animate(t):
+    #     pass
+    #     # pos_a.set_offsets([Zactual_a[0, t], Zactual_a[1, t]])
+    #     # pos_b.set_offsets([Zactual_b[0, t], Zactual_b[1, t]])
+
+    # ani = animation.FuncAnimation(fig,
+    #                                 animate,
+    #                                 save_count=T,  # total number of calls to animate
+    #                                 interval=dt * 1000)  # interval = miliseconds between frames
+    # ani.save("tag.mp4")
+
 
 
 
 """ Driver code """
-startA = [1, 1, 1]
+startA = [5, 5, 5] # blue
 goalA = [0, 0, 0]
-startB = [0, 0, 0]
-goalB = [1, 1, 1]
-cx = [0, 0, -3, 3, 0]
+startB = [0, 0, 0] # orange
+goalB = [5, 5, 5]
+cx = [0, 0, -3, 3, 0] # TODO change these?
 cy = [3, -3, 3, -3, 0]
 cz = [-3, 3, 0, 0, 0]
+sim(startA, goalA, startB, goalB, cx, cy, cz)
 
 # generate trajectories
-trajA = generateTrajectories(startA, goalA, cx, cy, cz)
-trajB = generateTrajectories(startB, goalB, cx, cy, cz)
-print(trajA.shape)
-print(trajB.shape)
+
+# print(trajA.shape, accA.shape)
+# print(trajB.shape, accB.shape)
 
 # get costs
-costs = getCostMats(trajA, trajB)
-print(costs)
+# costs = getCostMats(trajA, trajB)
+# print(f"costs[0] shape {costs[0].shape}")
+# print(costs)
 
-# solve the LCPs
-sols = solveLCPs(costs)
-print(f"solution shape {sols.shape}")
-print(sols)
+# # solve the LCPs
+# sols = solveLCPs(costs)
+# print(f"solution shape {sols.shape}")
+# print(sols)
+# print(sols.shape)
+# print(type(sols))
 
-asol = np.argmax(sols[0,0])
-bsol = np.argmax(sols[0,1])
-print(asol, bsol)
+# asol = np.argmax(sols[0,0])
+# bsol = np.argmax(sols[0,1])
+# print(asol, bsol)
 
-print("done")
+# print("done")
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-ax.plot(trajA[asol,0, :], trajA[asol,1,:], trajA[asol,2,:], label="A")
-ax.plot(trajB[bsol,0, :], trajB[bsol,1,:], trajB[bsol,2,:], label="B")
-ax.legend(loc="upper left")
-plt.show()
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# ax.plot(trajA[asol,0, :], trajA[asol,1,:], trajA[asol,2,:], label="A")
+# ax.plot(trajB[bsol,0, :], trajB[bsol,1,:], trajB[bsol,2,:], label="B")
+# ax.legend(loc="upper left")
+# plt.show()
 
 
 
---
+++ lemke_howson.py
@@ -0,0 +1,126 @@
+# HW1_sol.py
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.animation as animation
+import copy
+
+
+def LCP_lemke_howson(A, B, max_iters=50):
+    # Function for computing solutions to the Bimatrix game defined by cost matrices A > 0, B > 0
+    # Returns a tuple (x,y), where x and y are the mixed equilibrium strategies for player 1 and player 2, respectively.
+
+    # This function is an implementation of the Lemke-Howson method, as described in Section 2.5 of
+    # "Linear Complementarity, Linear and Nonlinear Programming", by Katta G. Murty
+    n = A.shape[0]
+    m = A.shape[1]
+
+    # Ensure A and B are strictly positive
+    if (np.any(A <= 0)): # add separately to handle ufunc error
+        A = A + abs(np.min(A)) + float(1.0) # any positive constant would do
+    if (np.any(B <= 0)):
+        B = B + abs(np.min(B)) + float(1.0) # any positive constant would do
+    
+    M = np.vstack((np.hstack((np.zeros((n,n)), A)), np.hstack((B.T, np.zeros((m,m))))))
+    T = np.hstack((np.eye(n+m), -M, -np.ones((n+m, 1))))
+    basis = np.arange(n+m)
+
+    enter_idx = n + m # start with last column of T corresponding to q
+    t = np.argmin(-T[n:n+m, enter_idx]) + n
+    basis[t] = enter_idx # update basis
+
+    pivot = T[t, :] / T[t, enter_idx] # similar to above, assign entering var st exiting var is 0
+    T -= np.outer(T[:, enter_idx], pivot) # row reduce
+    T[t, :] = pivot
+
+    enter_idx = t + n + m # next entering variable, complement of previous
+    t = np.argmin(-T[0:n, enter_idx])
+    basis[t] = enter_idx
+
+    pivot = T[t, :] / T[t, enter_idx]
+    T -= np.outer(T[:, enter_idx], pivot)
+    T[t, :] = pivot
+
+    enter_idx = t+n+m
+    xi1 = n+m
+    u1 = 0
+
+    T_start = copy.deepcopy(T)
+
+    it, ret = 0, 1 # assume no ray termination
+    while (xi1 in basis and u1 in basis and it < max_iters):
+        it +=1
+        d = T[:, enter_idx]
+        wrong_dir = d <= 0
+        ratios = np.zeros(n+m)
+
+        for i in range(n+m):
+            if d[i] > 0: # get ratios
+                ratios[i] = T[i, -1] / d[i]
+        ratios[wrong_dir] = np.inf
+
+        t = np.argmin(ratios)
+        minRat = ratios[t]
+        doLex = True
+
+        if np.sum(np.min(ratios) == minRat) > 1: # check for duplicates 
+            if ratios[np.where(basis == u1)] == minRat:
+                t = np.where(basis == u1)[0][0]
+                doLex = False
+            elif ratios[np.where(basis == xi1)] == minRat:
+                t = np.where(basis == xi1)[0][0]
+                doLex = False
+            else:
+                B_inv = np.linalg.inv(T_start[:, basis])
+
+            candidate_idx = [] # candidate indices
+            for i in range(n):
+                if d[i] > 0 and ratios[i] == np.min(ratios): # ignore non-positive values
+                    candidate_idx.append(i)
+
+            k = 0
+            while doLex and np.sum(ratios == np.min(ratios)) > 1 and k < n+m:
+                wrong_dir = d[candidate_idx] <= 0
+                ratios = B_inv[candidate_idx, i] / d[candidate_idx]
+                ratios[wrong_dir] = np.inf
+                t = candidate_idx[np.argmin(ratios)] # store current min
+                throwaway_idx = [] # remove idxs that dont correspond to min
+                for i in range(len(candidate_idx)):
+                    if ratios[i] != np.min(ratios):
+                        throwaway_idx.append(candidate_idx[i])
+                for j in range(len(throwaway_idx)):
+                    candidate_idx.remove(throwaway_idx[j])
+                k += 1
+
+        if ~ np.all(wrong_dir):
+                # This block of code assigns entering var necessary value so that exiting var is 0
+                pivot = T[t, :] / T[t, enter_idx]
+                T -= np.outer(T[:, enter_idx], pivot)
+                T[t, :] = pivot
+                #
+                exiting_ind = basis[t]
+                basis[t] = enter_idx
+                if exiting_ind >= n+m:
+                    enter_idx = exiting_ind - n - m
+                else:
+                    enter_idx = exiting_ind + n + m
+        else:
+            ret = 0
+            print("ray termination")
+            break
+
+    if ret:
+        vars = np.zeros((m+n, 1))
+        align = basis % (n+m) >= 0
+        vars[basis[align] % (n+m), 0] = T[align, -1]
+        xi = vars[0:n]
+        eta = vars[n:m+n]
+        x = xi / np.sum(xi)
+        y = eta / np.sum(eta)
+        return (x, y)
+    else:
+        x = np.zeros((n, 1))
+        y = np.zeros((m, 1))
+        return (x, y) 
+
+
